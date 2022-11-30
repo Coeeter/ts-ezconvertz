869c310962b7e3335d56e20c936c0494e16e81c6
Nasrullah <nasrullah01n@gmail.com>
2022-11-30 20:55:56 +0800

    Giving html file from server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
+import path from 'path';
 import envConfig from './config/EnvConvig';
 import ConverterController from './controllers/ConverterController';
 
@@ -7,10 +8,16 @@ const app: Express = express();
 app.use(require('cors')());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '..', 'public', 'dist')));
 
 const controller = new ConverterController();
 app.post('/api/get-data', controller.getDataOfVideo);
 app.post('/api/convert', controller.convertAndDownloadAudioAsZip);
+
+app.get('/', (req: Request, res: Response) => {
+  const pathToFile = path.join(__dirname, '..', 'public', 'dist', 'index.html');
+  res.sendFile(pathToFile);
+});
 
 app.listen(envConfig.server.port, () => {
   console.log(`Server listening on port ${envConfig.server.port}`);
